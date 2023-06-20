5f3d960408b4cb190b5f26e938a7d38ff2152b49
NARUTO9556 <gruver5263@gmail.com>
2023-06-20 15:14:53 +0800

    промежуточный коммит

--- src/main/java/com/example/calculator/CalculatorService.java
+++ src/main/java/com/example/calculator/CalculatorService.java
@@ -6,5 +6,24 @@ import static org.apache.coyote.http11.Constants.a;
 
 @Service
 public class CalculatorService {
+    public String hello() {
+        return "Добро пожаловать в калькулятор";
+    }
 
+    public String plus(int num1, int num2) {
+        int sum = num1 + num2;
+        return String.format("%s + %s = %s",num1,num2,sum);
+    }
+    public String minus (int num1, int num2) {
+        int sum = num1 - num2;
+        return String.format("%s - %s = %s",num1,num2,sum);
+    }
+    public String multiply(int num1, int num2) {
+        int sum = num1 * num2;
+        return String.format("%s * %s = %s",num1,num2,sum);
+    }
+    public String divide(int num1, int num2) {
+        int sum = num1 / num2;
+        return String.format("%s * %s = %s",num1,num2,sum);
+    }
 }
