d5293fc0894a21c8104daa5863801d43f13b07b5
NARUTO9556 <gruver5263@gmail.com>
2023-06-22 02:23:29 +0800

    spring boot+maven

--- src/main/java/com/example/calculator/CalculatorService.java
+++ src/main/java/com/example/calculator/CalculatorService.java
@@ -22,9 +22,8 @@ public class CalculatorService {
         return String.format("%s * %s = %s",num1,num2,sum);
     }
     public String divide(int num1, int num2) {
-        int sum = num1 / num2;
         if (num2 != 0) {
-            return String.format("%s / %s = %s", num1, num2, sum);
+            return String.format("%s / %s = %s", num1, num2, num1 / num2);
         } else {
             return "На ноль делить нельзя!";
         }
